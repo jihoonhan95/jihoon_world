78ccb6b3c5af1b00518b50d0fffe3724666d514a
한지훈 <uuiop0129@gmail.com>
2022-11-02 11:18:37 +0900

    11.02

--- portfolio/programming/assets/development/shoes/programming/js/slidshow.js
+++ portfolio/programming/assets/development/shoes/programming/js/slidshow.js
@@ -81,76 +81,76 @@ function nextMove() {
     // // 슬라이드 이동 시 현재 활성화된 pagination 변경
     paginationItems.forEach((i) => i.classList.remove("active"));
     paginationItems[currSlide - 1].classList.add("active");
-  }
+    }
 }
 function prevMove() {
-  currSlide--;
+    currSlide--;
   // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
-  if (currSlide > 0) {
+    if (currSlide > 0) {
     // 슬라이드를 이동시키기 위한 offset 계산
     const offset = slideWidth * currSlide;
     // 각 슬라이드 아이템의 left에 offset 적용
     slideItems.forEach((i) => {
-      i.setAttribute("style", `left: ${-offset}px`);
+        i.setAttribute("style", `left: ${-offset}px`);
     });
     // 슬라이드 이동 시 현재 활성화된 pagination 변경
     paginationItems.forEach((i) => i.classList.remove("active"));
     paginationItems[currSlide - 1].classList.add("active");
-  } else {
+    } else {
     // 무한 슬라이드 기능 - currSlide 값만 변경해줘도 되지만 시각적으로 자연스럽게 하기 위해 아래 코드 작성
     currSlide = maxSlide + 1;
     let offset = slideWidth * currSlide;
     // 각 슬라이드 아이템의 left에 offset 적용
     slideItems.forEach((i) => {
-      i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
+        i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
     });
     currSlide--;
     offset = slideWidth * currSlide;
     setTimeout(() => {
       // 각 슬라이드 아이템의 left에 offset 적용
-      slideItems.forEach((i) => {
+        slideItems.forEach((i) => {
         // i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
         i.setAttribute("style", `transition: ${0.15}s; left: ${-offset}px`);
-      });
+        });
     }, 0);
     // 슬라이드 이동 시 현재 활성화된 pagination 변경
     paginationItems.forEach((i) => i.classList.remove("active"));
     paginationItems[currSlide - 1].classList.add("active");
-  }
+    }
 }
 
 // 버튼 엘리먼트에 클릭 이벤트 추가하기
 nextBtn.addEventListener("click", () => {
   // 이후 버튼 누를 경우 현재 슬라이드를 변경
-  nextMove();
+    nextMove();
 });
 // 버튼 엘리먼트에 클릭 이벤트 추가하기
 prevBtn.addEventListener("click", () => {
   // 이전 버튼 누를 경우 현재 슬라이드를 변경
-  prevMove();
+    prevMove();
 });
 
 // 브라우저 화면이 조정될 때 마다 slideWidth를 변경하기 위해
 window.addEventListener("resize", () => {
-  slideWidth = slide.clientWidth;
+    slideWidth = slide.clientWidth;
 });
 
 // 각 페이지네이션 클릭 시 해당 슬라이드로 이동하기
 for (let i = 0; i < maxSlide; i++) {
   // 각 페이지네이션마다 클릭 이벤트 추가하기
-  paginationItems[i].addEventListener("click", () => {
+    paginationItems[i].addEventListener("click", () => {
     // 클릭한 페이지네이션에 따라 현재 슬라이드 변경해주기(currSlide는 시작 위치가 1이기 때문에 + 1)
     currSlide = i + 1;
     // 슬라이드를 이동시키기 위한 offset 계산
     const offset = slideWidth * currSlide;
     // 각 슬라이드 아이템의 left에 offset 적용
     slideItems.forEach((i) => {
-      i.setAttribute("style", `left: ${-offset}px`);
+        i.setAttribute("style", `left: ${-offset}px`);
     });
     // 슬라이드 이동 시 현재 활성화된 pagination 변경
     paginationItems.forEach((i) => i.classList.remove("active"));
     paginationItems[currSlide - 1].classList.add("active");
-  });
+    });
 }
 
 // 드래그(스와이프) 이벤트를 위한 변수 초기화
@@ -164,13 +164,13 @@ slide.addEventListener("mousedown", (e) => {
 
 slide.addEventListener("mouseup", (e) => {
   endPoint = e.pageX; // 마우스 드래그 끝 위치 저장
-  if (startPoint < endPoint) {
+    if (startPoint < endPoint) {
     // 마우스가 오른쪽으로 드래그 된 경우
     prevMove();
-  } else if (startPoint > endPoint) {
+    } else if (startPoint > endPoint) {
     // 마우스가 왼쪽으로 드래그 된 경우
     nextMove();
-  }
+    }
 });
 
 // 모바일 터치 이벤트 (스와이프)
@@ -179,28 +179,28 @@ slide.addEventListener("touchstart", (e) => {
 });
 slide.addEventListener("touchend", (e) => {
   endPoint = e.changedTouches[0].pageX; // 터치가 끝나는 위치 저장
-  if (startPoint < endPoint) {
+    if (startPoint < endPoint) {
     // 오른쪽으로 스와이프 된 경우
     prevMove();
-  } else if (startPoint > endPoint) {
+    } else if (startPoint > endPoint) {
     // 왼쪽으로 스와이프 된 경우
     nextMove();
-  }
+    }
 });
 
 // 기본적으로 슬라이드 루프 시작하기
 let loopInterval = setInterval(() => {
-  nextMove();
+    nextMove();
 }, 2000);
 
 // 슬라이드에 마우스가 올라간 경우 루프 멈추기
 slide.addEventListener("mouseover", () => {
-  clearInterval(loopInterval);
+    clearInterval(loopInterval);
 });
 
 // 슬라이드에서 마우스가 나온 경우 루프 재시작하기
 slide.addEventListener("mouseout", () => {
-  loopInterval = setInterval(() => {
+    loopInterval = setInterval(() => {
     nextMove();
-  }, 3000);
+    }, 3000);
 });
